eed4e1205bfcc96152d350dc8dce31a7de961827
Rustam Shorov <srmparusa@gmail.com>
2019-01-02 21:08:54 +0300

    Added required files and first preparations

---
+++ Match-Match_Game_MemoryWarmUp/Model/CardModel.swift
@@ -0,0 +1,9 @@
+//
+//  CardModel.swift
+//  Match-Match_Game_MemoryWarmUp
+//
+//  Created by Rustam Shorov on 02.01.2019.
+//  Copyright © 2019 Rustam Shorov. All rights reserved.
+//
+
+import Foundation
