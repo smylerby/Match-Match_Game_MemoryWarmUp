c862eaaeefc4e72991594488b6d67bdd4f886ea1
Rustam Shorov <srmparusa@gmail.com>
2019-01-10 13:40:17 +0300

    Finished MusicGame but with some mistakes

--- Match-Match_Game_MemoryWarmUp/Controller/MemorizeSoundsViewController.swift
+++ Match-Match_Game_MemoryWarmUp/Controller/MemorizeSoundsViewController.swift
@@ -12,15 +12,12 @@ import AVFoundation
 
 class MemorizeSoundsViewController: UIViewController {
 
-    @IBOutlet weak var playButtonPressed: UIButton!
-    @IBOutlet var soundButton: [UIButton]!
-    @IBOutlet weak var levelLabel: UILabel!
-//    
-//    var sound1Player: AVAudioPlayer!
-//    var sound2Player: AVAudioPlayer!
-//    var sound3Player: AVAudioPlayer!
-//    var sound4Player: AVAudioPlayer!
+    @IBOutlet var soundButtonArray: [UIButton]!
     
+    @IBOutlet weak var playButtonPressed: UIButton!
+    
+    @IBOutlet weak var levelLabel: UILabel!
+
     var playlist = [Int]()
     var currentItem = 0
     var numbersOfTaps = 0
@@ -30,65 +27,140 @@ class MemorizeSoundsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        setUpAudioFiles()
-//
         
     }
     
-//    func setUpAudioFiles() {
-//
-//        let soundFilePath = Bundle.main.path(forResource: "1", ofType: "wav")
-//        let soundFileUrl = URL(fileURLWithPath: soundFilePath!)
-//
-//        let soundFilePath2 = Bundle.main.path(forResource: "2", ofType: "wav")
-//        let soundFileUrl2 = URL(fileURLWithPath: soundFilePath2!)
-//
-//        let soundFilePath3 = Bundle.main.path(forResource: "3", ofType: "wav")
-//        let soundFileUrl3 = URL(fileURLWithPath: soundFilePath3!)
-//
-//        let soundFilePath4 = Bundle.main.path(forResource: "4", ofType: "wav")
-//        let soundFileUrl4 = URL(fileURLWithPath: soundFilePath4!)
-//
-//        do {
-//            try sound1Player = AVAudioPlayer(contentsOf: soundFileUrl)
-//            try sound2Player = AVAudioPlayer(contentsOf: soundFileUrl2)
-//            try sound3Player = AVAudioPlayer(contentsOf: soundFileUrl3)
-//            try sound4Player = AVAudioPlayer(contentsOf: soundFileUrl4)
-//        } catch {
-//            print(error)
-//        }
-//
-//        sound1Player.delegate = self
-//        sound2Player.delegate = self
-//        sound3Player.delegate = self
-//        sound4Player.delegate = self
-//
-//        sound1Player.numberOfLoops = 0
-//        sound2Player.numberOfLoops = 0
-//        sound3Player.numberOfLoops = 0
-//        sound4Player.numberOfLoops = 0
-//
-//    }
-    
-
     @IBAction func soundButtonPressed(_ sender: UIButton) {
         
-        switch sender.tag {
+        if readyForUser {
+            switch sender.tag {
+            case 1:
+                SoundManager.playSound(.red)
+            case 2:
+                SoundManager.playSound(.yellow)
+            case 3:
+                SoundManager.playSound(.blue)
+            case 4:
+                SoundManager.playSound(.green)
+            default:
+                break
+            }
+        }
+    }
+    
+    func checkIfCorrect (buttonPressed: Int) {
+        if buttonPressed == playlist[numbersOfTaps] {
+            if numbersOfTaps == playlist.count - 1 {
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.7) {
+                    self.nextRound()
+                }
+                return
+            }
+            numbersOfTaps += 1
+        } else {
+            resetGame()
+            //GameOver
+        }
+    }
+    
+    func resetGame() {
+        level = 1
+        readyForUser = false
+        numbersOfTaps = 0
+        playlist = []
+        levelLabel.text = "Game Over"
+        playButtonPressed.isHidden = false
+    }
+    
+    func nextRound() {
+        level += 1
+        levelLabel.text = "Level \(level)"
+        readyForUser = false
+        numbersOfTaps = 0
+        currentItem = 0
+        disableButtons()
+        let randomNumber = Int(arc4random_uniform(4) + 1)
+        playlist.append(randomNumber)
+        playNextItem()
+        //diable Button
+    }
+    
+    @IBAction func playButtonPressedAction(_ sender: UIButton) {
+        levelLabel.text = "Level 1"
+        disableButtons()
+        let randomNumber = Int(arc4random_uniform(4) + 1)
+        playlist.append(randomNumber)
+        playButtonPressed.isHidden = true
+        playNextItem()
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if currentItem <= playlist.count - 1 {
+            playNextItem()
+        } else {
+            readyForUser = true
+            resetButtonHighLights()
+            enableButtons()
+        }
+    }
+    
+    func playNextItem() {
+        
+        let selectedItem = playlist[currentItem]
+        highlightButtonWith(tag: selectedItem)
+        
+        switch selectedItem {
         case 1:
             SoundManager.playSound(.red)
+            break
         case 2:
             SoundManager.playSound(.yellow)
+            break
         case 3:
             SoundManager.playSound(.blue)
+            break
         case 4:
             SoundManager.playSound(.green)
+            break
+        default:
+            break
+        }
+        currentItem += 1
+    }
+    
+    func highlightButtonWith(tag: Int) {
+        resetButtonHighLights()
+        
+        switch tag {
+        case 1:
+            soundButtonArray[tag - 1].setImage(UIImage(named: "redPressed"), for: .normal)
+        case 2:
+            soundButtonArray[tag - 1].setImage(UIImage(named: "yellowPressed"), for: .normal)
+        case 3:
+            soundButtonArray[tag - 1].setImage(UIImage(named: "bluePressed"), for: .normal)
+        case 4:
+            soundButtonArray[tag - 1].setImage(UIImage(named: "greenPressed"), for: .normal)
         default:
             break
         }
     }
+    func resetButtonHighLights() {
+        soundButtonArray[0].setImage(UIImage(named: "red"), for: .normal)
+        soundButtonArray[1].setImage(UIImage(named: "yellow"), for: .normal)
+        soundButtonArray[2].setImage(UIImage(named: "blue"), for: .normal)
+        soundButtonArray[3].setImage(UIImage(named: "green"), for: .normal)
+    }
+    func disableButtons() {
+        for button in soundButtonArray {
+            button.isUserInteractionEnabled = false
+        }
+    }
     
-    
+    func enableButtons() {
+        for button in soundButtonArray {
+            button.isUserInteractionEnabled = true
+        }
+    }
 }
 
 extension MemorizeSoundsViewController: AVAudioPlayerDelegate {
