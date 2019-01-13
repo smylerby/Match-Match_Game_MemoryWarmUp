a726cc95704420bff09b635688ec89844446453c
Rustam Shorov <srmparusa@gmail.com>
2019-01-13 21:35:53 +0300

    added additional VC for choose a game

---
+++ Match-Match_Game_MemoryWarmUp/Controller/GamePickerViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GamePickerViewController.swift
+//  Match-Match_Game_MemoryWarmUp
+//
+//  Created by Rustam Shorov on 13.01.2019.
+//  Copyright © 2019 Rustam Shorov. All rights reserved.
+//
+
+import UIKit
+
+class GamePickerViewController: UIViewController {
+
+    @IBOutlet weak var playerNameLabel: UILabel!
+    
+    @IBAction func startMatchGameButton(_ sender: UIButton) {
+        
+        
+        
+    }
+    @IBAction func startMemorizeSoundsGameButton(_ sender: UIButton) {
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
