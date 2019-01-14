7db08328753ff604f64d9be7cc96bedfc94fda2e
Rustam Shorov <srmparusa@gmail.com>
2019-01-14 16:05:39 +0300

    Change the design of gamePickerVC

--- Match-Match_Game_MemoryWarmUp/Controller/GamePickerViewController.swift
+++ Match-Match_Game_MemoryWarmUp/Controller/GamePickerViewController.swift
@@ -11,40 +11,65 @@ import UIKit
 class GamePickerViewController: UIViewController {
     
     var playerName: String = ""
+    var game: String = ""
     
+    
+    @IBOutlet weak var matchGameOutlet: UIButton!
+    @IBOutlet weak var startButtonOutlet: UIButton!
+    @IBOutlet weak var gameImage: UIImageView!
     @IBOutlet weak var playerNameLabel: UILabel!
+    @IBOutlet weak var memorizeGameOutlet: UIButton!
     
     @IBAction func startMatchGameButton(_ sender: UIButton) {
+        memorizeGameOutlet.alpha = 0.3
+        sender.alpha = 1
+        gameImage.image = UIImage(named: "match")
+        game = "Match"
+        startButtonOutlet.alpha = 1
+        startButtonOutlet.isUserInteractionEnabled = true
+    }
+    
+    @IBAction func startGameButton(_ sender: UIButton) {
+        if game == "Match" {
+            showMatchGame()
+        } else {
+            showMemorizeGame()
+        }
+    }
+    
+    @IBAction func startMemorizeSoundsGameButton(_ sender: UIButton) {
+        matchGameOutlet.alpha = 0.3
+        sender.alpha = 1
+        gameImage.image = UIImage(named: "memorize")
+        game = "Memorize"
+        startButtonOutlet.alpha = 1
+        startButtonOutlet.isUserInteractionEnabled = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        gameImage.layer.borderWidth = 2
+        matchGameOutlet.alpha = 0.3
+        memorizeGameOutlet.alpha = 0.3
+        startButtonOutlet.alpha = 0.3
+        startButtonOutlet.isUserInteractionEnabled = false
+        
+        playerNameLabel.text = "\(playerName), choose your Game!"
+    }
+    
+    func showMatchGame() {
         
         if let matchGameVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MatchGameVC") as? MatchGameViewController {
-            
             matchGameVC.playersName = playerName
             navigationController?.pushViewController(matchGameVC, animated: true)
             
         }
     }
-    
-    @IBAction func startMemorizeSoundsGameButton(_ sender: UIButton) {
+    func showMemorizeGame() {
         
         if let memorizeSoundVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MemorizeSoundVC") as? MemorizeSoundsViewController {
             navigationController?.pushViewController(memorizeSoundVC, animated: true)
             
         }
-        
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        playerNameLabel.text = "\(playerName), choose your Game!"
-    }
-    
-//    func showTheGameWithIdentifier(_ identifier: String, controller: UIViewController) {
-//
-//        if let game = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: identifier) as? controller {
-//            navigationController?.pushViewController(game, animated: true)
-//
-//        }
-//    }
-    
 }
